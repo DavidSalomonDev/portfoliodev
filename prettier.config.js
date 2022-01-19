838a0d88367beb4242598e64ca1a0e1eeac61695
salo <salo@david-salomon.com>
2022-01-19 11:30:20 -0600

    :tada: begin project

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+	arrowParens: true,
+	singleQuote: false,
+	bracketSpacing: true,
+	endOfLine: "lf",
+	semi: true,
+	tabWidth: 2,
+	trailingComma: 'none'
+}
+
+module.exports = options
